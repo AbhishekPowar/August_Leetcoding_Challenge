6b8efe0853637a0616f3fa30d611d9a6ccaebe3b
abhishek <mailabhipoear@gmail.com>
2020-08-21 19:16:18 +0530

    explanation added

--- day20_sort_array_by_parity.py
+++ day20_sort_array_by_parity.py
@@ -10,7 +10,8 @@ class Solution:
         return A
 # Explanation:
 # R increments regardless
-# L increments only when it is 0 or swap
+# L increments only when it is 1 or swap
+# Why 1 because odd%2==1
 # Pass 0:
 # low = v
 # A   = 0 0 1 1 0 1 0
@@ -52,4 +53,52 @@ class Solution:
 # A   = 0 0 0 0 1 1 1
 # r   =             ^
 # # SWAP
+# ----------------
+
+
+# Explanation:
+# R increments regardless
+# L increments only when it is 1 or swap
+# Why 1 because odd%2==1
+# Pass 0:
+# low = v
+# A   = 0 0 1 1 0 1 0
+# r   = ^
+# ----------------
+# Pass 2:
+# low =   v
+# A   = 0 0 1 1 0 1 0
+# r   =   ^
+# ----------------
+# Pass 3:
+# low =     v
+# A   = 0 0 1 1 0 1 0
+# r   =     ^
+# ----------------
+# Pass 4:
+# low =     v
+# A   = 0 0 1 1 0 1 0
+# r   =       ^
+# ----------------
+# Pass 5:
+# low =     v
+# A   = 0 0 1 1 0 1 0
+# r   =         ^
+# SWAP
+# ----------------
+# Pass 6:
+# low =       v
+# A   = 0 0 0 1 1 1 0
+# r   =           ^
+# ----------------
+# Pass 7:
+# low =       v
+# A   = 0 0 0 1 1 1 0
+# r   =             ^
+# ----------------
+# Pass 8:
+# low =       v
+# A   = 0 0 0 0 1 1 1
+# r   =             ^
+# SWAP
 # ----------------
